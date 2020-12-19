bcf8bba03590b0be7965b257b79fcd9c7b755f84
Deanira F <52442666+deanira@users.noreply.github.com>
2020-12-19 10:41:39 +0700

    fixing mapView

--- app/src/main/java/com/azizapp/test/HomeFragment.kt
+++ app/src/main/java/com/azizapp/test/HomeFragment.kt
@@ -7,15 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
-import kotlinx.android.synthetic.main.home1.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
         val map = ArcGISMap(Basemap.Type.OPEN_STREET_MAP, 0.5695578669076772, 101.42584214573947, 14)
         mapView.map = map
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
