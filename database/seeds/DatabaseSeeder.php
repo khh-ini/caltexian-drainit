e9b79071e7f1cd2917a5c274d9eddfc2b6d57a7f
Kiki Harapan Hutapea <kiki18ti@mahasiswa.pcr.ac.id>
2021-06-19 09:26:53 +0700

    fix db seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(
+        $this->call([
             AdminSeeder::class,
             MasyarakatSeeder::class,
-            PetugasSeeder::class
-        );
+            PetugasSeeder::class,
+        ]);
     }
 }
